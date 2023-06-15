138d82ff1bd6062355fd7c52a2d29e0da5ece48e
Ammar Siddiqui <ammars22@pitzer-rw02.ten.osc.edu>
2023-06-15 14:36:11 -0400

    part 2

--- public/Am.js
+++ public/Am.js
@@ -1,10 +1,25 @@
-function LD(){
-    if(body.backgroundColor = rgb(0,0,0)){
-    document.body.style.backgroundColor = rgb(255,255,255);
+function LD(event){
+    console.log(event);
+   // if(div.backgroundColor = rgb(0,0,0)){
+  
+    nav = document.getElementById("nav");//.style.backgroundColor = rgb(23,23,83);
+
+    list = nav.classList;
+    arrayNav = Array.from(list)
+
+    if (arrayNav.includes('bg-success')){
+        list.remove('bg-success');
+        list.add('bg-light');
     }
     else{
-      
-        document.body.style.backgroundColor = rgb(0,0,0);
-    
+        list.remove('bg-light');
+        list.add('bg-success')
+        
     }
+   //}
+   // else{
+      
+        //document.div.style.backgroundColor = rgb(0,0,0);
+    
+//}
 }
