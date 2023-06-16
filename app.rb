67c913c897b41e0bd2ec67129e80b30dc20a7ef8
Ammar Siddiqui <ammars22@owens-rw01.ten.osc.edu>
2023-06-16 15:49:34 -0400

    Part 6

--- app.rb
+++ app.rb
@@ -32,6 +32,44 @@ class App < Sinatra::Base
   def project_dirs
     Dir.children(projects_root).reject { |dir| dir == 'input_files' }.sort_by(&:to_s)
   end
+
+  def input_files_dir
+    "#{projects_root}/input_files"
+  end
+
+  def copy_upload(input: nil, output:nil)
+    input_sha = Pathname.new(input).file? ? Digest::SHA256.file(input) : nil
+    output_sha = Pathname.new(output).file? ? Digest::SHA256.file(output) : nil
+    return if input_sha.to_s == output_sha.to_s
+    File.open(output, 'wb') do |f|
+      f.write(input.read)
+    end
+  end
+
+  def job_state(job_id)
+    state = `/bin/squeue -j #{job_id} -h -o '%t'`.chomp
+    s = {
+      '' => 'Completed',
+      'R' => 'Running',
+      'C' => 'Completed',
+      'Q' => 'Queued',
+      'CF' => 'Queued',
+      'PD' => 'Queued'
+
+    }[state]
+
+    s.nil? ? 'Unknown' : s
+  end
+  def badge(state)
+    {
+      '' => 'warning',
+      'Unknown' => 'warning',
+      'Running' => 'success',
+      'Queued' => 'info',
+      'Completed' => 'primary',
+    }[state.to_s]
+  end
+  
   get '/' do
     logger.info('requsting the index')
     @flash = session.delete(:flash) || { info:'Welcome to SI'}
@@ -43,27 +81,59 @@ class App < Sinatra::Base
     if params[:dir] == "new" || params[:dir] == 'input_files'
       erb(:new_project)
     else
+
       @dir = Pathname("#{projects_root}/#{params[:dir]}")
       @flash = session.delete(:flash)
-      @upload_blend_files = Dir.glob("#{input_files_dir}/*.blend").map { |f| File.basename(f) }
+      @uploaded_blend_files = Dir.glob("#{input_files_dir}/*.blend").map { |f| File.basename(f) }
       @project_name = @dir.basename.to_s.gsub('_',' ').capitalize
+
       unless @dir.directory? || @dir.readable?
         session[:flash] = { danger: "#{dir} does not exist"}
         redirect(url('/'))
       end
+
+    @images = Dir.glob("#{@dir}/*.png")
+    @frame_render_job_id = File.read("#{@dir}/.frame_render_job_id").chomp
+    @frame_render_job_state = job_state(@frame_render_job_id)
+    @frame_render_badge = badge(@frame_render_job_state)
     erb(:show_project)
     end
   end
   post '/projects/new' do
+
     logger.info{"We are currently trying to render frames with #{params.inspect}."}
     dir = params[:name].downcase.gsub(' ','_')
     "#{projects_root}/#{dir}".tap { |d| FileUtils.mkdir_p(d) }
     session[:flash] = {info: "made new project '#{params[:name]}'"}
     redirect(url("/projects/#{dir}"))
+
   end
   post '/render/frames' do
-    session[:flash] = {info: "rendering frames with '#{params}'"}
-    redirect(url("/"))
+
+    logger.info("Trying to render frames with '#{params.inspect}'")
+
+    if params['blend_file'].nil?
+      blend_file = "#{input_files_dir}/#{params[:upload_blend_files]}"
+    else
+      blend_file = "#{input_files_dir}/#{params['blend_file'][:filename]}" 
+      copy_upload(input: params['blend_file'][:tempfile], output: blend_file)
+    end
+
+    dir=params[:dir]
+    basename = File.basename(blend_file, '.*')
+    walltime = format('%02d:00:00', params[:num_hours])
+    args = ['-J', "blender-#{basename}", '--parsable','-A', params[:project_name]]
+    args.concat ['--export',"BLEND_FILE_PATH=#{blend_file},OUTPUT_DIR=#{dir},FRAMES_RANGE=#{params[:FRAMES_RANGE]}"]
+    args.concat ['-n',params[:num_cpus], '-t', walltime, "-M", 'pitzer']
+    args.concat ['--output', "#{dir}/frame-render-%j.out"]
+    output = `/bin/sbatch #{args.join(' ')} #{__dir__}/render_frames.sh 2>&1`
+    job_id = output.strip.split(';').first
+    `echo #{job_id} > #{dir}/.frame_render_job_id`
+    session[:flash] = { info: "submitted job#{job_id}"}
+    redirect(url("/projects/#{dir.split('/').last}"))
+    
+    
+
   end
   
 end
--- public/Am.js
+++ public/Am.js
@@ -1,40 +1,26 @@
 function LD(){
   
-   // if(div.backgroundColor = rgb(0,0,0)){
-  
-    nav = document.getElementById("nav");//.style.backgroundColor = rgb(23,23,83);
-    text = document.getElementById
-    list = nav.classList;
-    arrayNav = Array.from(list)
-    var count = 0
-    if (count==0){
-        list.remove('bg-success');
-        list.add('bg-light');
-        list.remove('nav-bar light');
-        list.add('nav-bar dark');
-        count+=1
-    }
-    else if(count==1){
-        list.remove('bg-light');
-        list.add('bg-warning');
-        count+=1;
-        
-    }
-    else if(count==2){
-        list.remove('bg-warning');
-        list.add('bg-danger');
-        count +=1;
-//navbar light
-    }
-    else if(count==3){
-        list.remove('bg-danger');
-        list.add('bg-dark');
-        count = 0;
-    }
-   //}
-   // else{
-      
-        //document.div.style.backgroundColor = rgb(0,0,0);
+       // if(div.backgroundColor = rgb(0,0,0)){
     
-//}
-}
+        nav = document.getElementById("nav");//.style.backgroundColor = rgb(23,23,83);
+        body = document.getElementById("body");
+        list = nav.classList;
+        arrayNav = Array.from(list);
+    
+        if (arrayNav.includes('bg-success')){
+            list.remove('bg-success');
+            list.add('bg-dark');
+            console.log('change;');
+        }
+        else{
+            list.remove('bg-dark');
+            list.add('bg-success');
+            console.log('change;');
+        }
+       //}
+       // else{
+    
+            //document.div.style.backgroundColor = rgb(0,0,0);
+    
+    //}
+    }
