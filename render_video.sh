c9f4dd4bb38df1d8dc5533588d2036809acce73a
Ammar Siddiqui <ammars22@pitzer-rw02.ten.osc.edu>
2023-06-19 15:20:23 -0400

    part 9.2

--- render_video.sh
+++ render_video.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 module load ffmpeg/4.0.2
-ffmpeg -r "$FRAMES_PER_SEC" -y -i "$FRAMES_DIR/render_%04d.png" -vsync vfr -b:v 16m -pix_fmp yuv420p "$FRAMES_DIR/video.mp4"
+set -x
+ffmpeg -r "$FRAMES_PER_SEC" -y -i "$FRAMES_DIR/render_%04d.png" -vsync vfr -b:v 16m -pix_fmt yuv420p "$FRAMES_DIR/video.mp4"
