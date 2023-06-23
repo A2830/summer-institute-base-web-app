26a61a41bdd9c59d74cb988a93e1e9ddad45b176
Ammar Siddiqui <ammars22@pitzer-rw02.ten.osc.edu>
2023-06-23 10:13:00 -0400

    part 9.10

--- render_video.sh
+++ render_video.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-module load ffmpeg/4.0.2
+#module load ffmpeg/4.0.2
 set -x
-ffmpeg -r "$FRAMES_PER_SEC" -y -i "$FRAMES_DIR/render_%04d.png" -vsync vfr -b:v 16m -pix_fmt yuv420p "$FRAMES_DIR/video.mp4"
+FRAMES_DIR=$(pwd)
+
+export PATH="$PATH:~ammars22/Public/ffmpeg/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:~ammars22/Public/ffmpeg/lib"
+
+ffmpeg -r "$FRAMES_PER_SEC" -y -i "$FRAMES_DIR/render_%04d.png" -vcodec libx264 video.mp4
+
